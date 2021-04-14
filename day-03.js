21e7e41a66361dc0633f6f4587295b469721d36a
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-14 20:38:18 +0300

    Create day-03.js

---
+++ day-03.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
+function trueOrFalse(val){
+  if (val) return "true";             
+  else  return "false";
+}
+
+//https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/javascript
+function saleHotdogs(n){
+//   if (n<5)  return 100*n
+//   else if (n>=5 && n<10) return 95*n
+//   else    return 90*n
+  return n<5 ? 100*n : n>=5 && n<10 ? 95*n : 90*n;
+}
+
+//https://www.codewars.com/kata/572059afc2f4612825000d8a/train/javascript
