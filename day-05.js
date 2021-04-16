06db1a69bfc4d42fedcf06e678928c769f8d500e
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-16 16:11:24 +0300

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -1,4 +1,10 @@
 // https://www.codewars.com/kata/is-this-my-tail/train/javascript
+function correctTail(body, tail) {
+  let sub = body.substr(body.length - tail.length);
+  if (sub === tail) {
+    return true;
+  } else return false;
+}
 
 // https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
 
