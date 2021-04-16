1413c2724167f2a13db79bbb87263b4f27cb1c20
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-16 16:18:50 +0300

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -7,5 +7,11 @@ function correctTail(body, tail) {
 }
 
 // https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+function bonusTime(salary, bonus) {
+  if(bonus){
+    return `£${salary*10}`;
+  }
+   else return "£" + salary;
+}
 
 // https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/javascript
