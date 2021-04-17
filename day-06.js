12e2805831614f30e63371e47d1e990d2277a376
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-17 12:42:43 +0300

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -1,1 +1,16 @@
+// https://www.codewars.com/kata/maximum-multiple/train/javascript
 
+// https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+function hero(bullets, dragons){
+  if(bullets - 2*dragons >= 0){
+    return true
+  } else return false
+}
+
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+function unusualFive() {
+let fiveStr = "Tadam";
+  return fiveStr.length;
+}
+
+// 
