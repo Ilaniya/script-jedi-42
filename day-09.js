122583bcd6d9bab97b9b139fffde09af474fc1a9
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-20 16:39:10 +0300

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -1,4 +1,14 @@
 // https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript
+function cutIt(arr) {
+  const tempArr = arr;
+  const min = Math.min(...tempArr.map(({ length }) => length));
+  for (let i = 0; i <= tempArr.length - 1; i++) {
+    if (tempArr[i].length > min) {
+      tempArr[i] = tempArr[i].substr(0, min);
+    }
+  }
+  return tempArr;
+}
 
 // https://www.codewars.com/kata/57277a31e5e51450a4000010/train/javascript
 
