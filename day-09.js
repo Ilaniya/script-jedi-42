f26f7b26d94f68652e4b6e5141fad28ecb674886
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-20 15:15:46 +0300

    Create day-09.js

---
+++ day-09.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript
+
+// https://www.codewars.com/kata/57277a31e5e51450a4000010/train/javascript
+
+// https://www.codewars.com/kata/57280481e8118511f7000ffa/train/javascript
