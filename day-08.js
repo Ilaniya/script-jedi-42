2cfc99cd5fad515d79858a3dcbfe4304c7d848e5
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-20 11:49:50 +0300

    Update day-08.js

--- day-08.js
+++ day-08.js
@@ -31,5 +31,31 @@ function whatNumberIsIt(n) {
 }
 
 // https://www.codewars.com/kata/57238ceaef9008adc7000603/train/javascript
+function colorOf(r, g, b) {
+  let rInSix = r.toString(16);
+  let gInSix = g.toString(16);
+  let bInSix = b.toString(16);
+  if (rInSix.length === 1) {
+    rInSix = 0 + rInSix;
+  }
+  if (gInSix.length === 1) {
+    gInSix = 0 + gInSix;
+  }
+  if (bInSix.length === 1) {
+    bInSix = 0 + bInSix;
+  }
+  return `#${rInSix}${gInSix}${bInSix}`;
+
+  // let color = new Object();
+  // color.r = r.toString(16);
+  // color.g = g.toString(16);
+  // color.b = b.toString(16);
+  //   for (let property in color){
+  //   if(color[property].length === 1){
+  //   color[property] = '0' + color[property];
+  // }
+  //     return `#${color.r}${color.g}${color.b}`
+  // }
+}
 
 // https://www.codewars.com/kata/57256064856584bc47000611/train/javascript
