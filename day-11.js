fdddc4d1e69052a962b1a4401958f31d457d2e3a
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-22 14:53:36 +0300

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -2,3 +2,31 @@
 
 
 // https://www.codewars.com/kata/5609fd5b44e602b2ff00003a/train/javascript
+function process2Arrays(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+  const resultArr = [];
+  let countElInBothArr = 0;
+  const countElInOneOfArr = new Set();
+  let countElLeftArr1 = 0;
+  let countElLeftArr2 = 0;
+  for (let value of set1) {
+    if (set2.has(value)) {
+      countElInBothArr++;
+    } else {
+      countElInOneOfArr.add(value);
+      countElLeftArr1++;
+    }
+  }
+  for (let value of set2) {
+    if (!set1.has(value)) {
+      countElLeftArr2++;
+      countElInOneOfArr.add(value);
+    }
+  }
+  resultArr.push(countElInBothArr);
+  resultArr.push(countElInOneOfArr.size);
+  resultArr.push(countElLeftArr1);
+  resultArr.push(countElLeftArr2);
+  return resultArr;
+}
