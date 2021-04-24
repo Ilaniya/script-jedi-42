6e7b58fd5a2635a460470f4c4a98f89c6f70d9a3
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-24 13:24:25 +0300

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -1,23 +1,24 @@
 // https://www.codewars.com/kata/maximum-multiple/train/javascript
-function maxMultiple(divisor, bound){
-    for(let i = bound; i>=divisor; i--){
-    if(i%divisor === 0){
+function maxMultiple(divisor, bound) {
+  for (let i = bound; i >= divisor; i--) {
+    if (i % divisor === 0) {
       return i;
     }
   }
 }
 
 // https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
-function hero(bullets, dragons){
-  if(bullets - 2*dragons >= 0){
-    return true
-  } else return false
+function hero(bullets, dragons) {
+  if (bullets - 2 * dragons >= 0) {
+    return true;
+  } else return false;
 }
 
 // https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
 function unusualFive() {
-const fiveStr = "Tadam";
+  const fiveStr = "Tadam";
   return fiveStr.length;
 }
 
-// 
+
+
