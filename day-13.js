5f911f063db80b5cca1659231354427cc13ef15d
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-25 13:51:48 +0300

    Update day-13.js

--- day-13.js
+++ day-13.js
@@ -1,4 +1,15 @@
 // https://www.codewars.com/kata/56c22c5ae8b139416c00175d/train/javascript
+function match(candidate, job) {
+  if (candidate.minSalary === undefined || job.maxSalary === undefined) {
+    throw new Error("Salary is absent");
+  }
+  if (candidate.minSalary - candidate.minSalary / 10 <= job.maxSalary) {
+    return true;
+  }
+  if (candidate.minSalary - candidate.minSalary / 10 > job.maxSalary) {
+    return false;
+  }
+}
 
 
 // https://www.codewars.com/kata/56c2578be8b139bd5c001bd8/train/javascript
