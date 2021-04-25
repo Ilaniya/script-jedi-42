1649bd7af2c0ba0757714aeeaf44835ab99a5230
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-25 22:56:50 +0300

    Update day-13.js

--- day-13.js
+++ day-13.js
@@ -7,3 +7,46 @@ function match(candidate, job) {
 }
 
 // https://www.codewars.com/kata/56c2578be8b139bd5c001bd8/train/javascript
+function matchLocations(candidate, jobLocations) {
+  const candLocations = [candidate.currentLocation];
+  for (let i = 0; i < candidate.desiredLocations.length; i += 1) {
+    candLocations.push(candidate.desiredLocations[i]);
+  }
+  for (let i = 0; i < candLocations.length; i += 1) {
+    if (jobLocations.includes(candLocations[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+function matchEquity(candidateEquity, jobEquity) {
+  if (candidateEquity === true && jobEquity > 0) {
+    return true;
+  }
+  if (candidateEquity === false && jobEquity === 0) {
+    return true;
+  }
+  if (candidateEquity === false && jobEquity > 0) {
+    return true;
+  }
+  return false;
+}
+
+function matchCandidate(candidate, job) {
+  const isEquityMatch = matchEquity(candidate.desiresEquity, job.equityMax);
+  const matchLocationsResult = matchLocations(candidate, job.locations);
+  const result = isEquityMatch && matchLocationsResult;
+  return result;
+}
+
+function match(job, candidates) {
+  const resultArr = [];
+  for (let i = 0; i < candidates.length; i += 1) {
+    const isCandidateSuitable = matchCandidate(candidates[i], job);
+    if (isCandidateSuitable) {
+      resultArr.push(candidates[i]);
+    }
+  }
+  return resultArr;
+}
+
