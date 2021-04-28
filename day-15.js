1bc01a9f09ac770a64783731691db384dd38c0a5
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-28 12:21:40 +0300

    Create day-15.js

---
+++ day-15.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/572ab0cfa3af384df7000ff8
+
+
+// https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50/
+
+
+// https://www.codewars.com/kata/572af273a3af3836660014a1
