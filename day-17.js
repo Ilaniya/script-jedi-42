9b6edf02e21a60e1bc4d4a65941b5b4cc792fde3
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-29 22:24:52 +0300

    Create day-17.js

---
+++ day-17.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/573023c81add650b84000429
+
+
+// https://www.codewars.com/kata/57308546bd9f0987c2000d07
+
+
+// https://www.codewars.com/kata/5731861d05d14d6f50000626
