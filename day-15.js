35ebd409d0fe7961e15c9016b1adad51378ab29f
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-29 16:34:31 +0300

    Update day-15.js

--- day-15.js
+++ day-15.js
@@ -35,3 +35,39 @@ function loopArr(arr, direction, steps) {
 
 
 // https://www.codewars.com/kata/572af273a3af3836660014a1
+function sliceArray(startArray, arrSizePart) {
+  const arrCut = startArray;
+  const resultArr = [];
+  for (let i = 0; i < arrSizePart.length; i += 1) {
+    resultArr.push(arrCut.slice(0, arrSizePart[i]));
+    arrCut.splice(0, arrSizePart[i]);
+  }
+  return resultArr;
+}
+
+function loopArr(arr, direction, steps) {
+  const arrCopy = arr;
+  for (let i = 0; i < steps; i += 1) {
+    if (direction === "left") {
+      const elem = arrCopy.shift();
+      arrCopy.push(elem);
+    }
+    if (direction === "right") {
+      const elem = arrCopy.pop();
+      arrCopy.unshift(elem);
+    }
+  }
+  return arrCopy;
+}
+
+function infiniteLoop(arr, d, n) {
+  let arrConcat = [];
+  const arrPartLength = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    arrConcat = arrConcat.concat(arr[i]);
+    arrPartLength.push(arr[i].length);
+  }
+  const arrLoop = loopArr(arrConcat, d, n);
+  const resultArr = sliceArray(arrLoop, arrPartLength);
+  return resultArr;
+}
