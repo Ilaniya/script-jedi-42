34a494e0c877f1c407e27bf36e5bf246685b23e9
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-29 16:56:17 +0300

    Create day-16.js

---
+++ day-16.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/572cb264362806af46000793
+
+
+// https://www.codewars.com/kata/572df796914b5ba27c000c90
+
+
+// https://www.codewars.com/kata/572fdeb4380bb703fc00002c
