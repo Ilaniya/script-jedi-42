86381102e1dac194fc5bf843ef2e1c7404916281
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-04-30 22:08:28 +0300

    Update day-18.js

--- day-18.js
+++ day-18.js
@@ -31,3 +31,12 @@ function tailAndHead(arr) {
 
 
 // https://www.codewars.com/kata/5732b0351eb838d03300101d/train/javascript
+function blackAndWhite(arr) {
+  if (!Array.isArray(arr)) {
+    return "It's a fake array";
+  }
+  if (arr.indexOf(5) !== -1 && arr.indexOf(13) !== -1) {
+    return "It's a black array";
+  }
+  return "It's a white array";
+}
