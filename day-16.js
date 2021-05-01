55fbc65257d2861af189b6edf7fffc5990242f84
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-02 00:03:23 +0300

    Update day-16.js

--- day-16.js
+++ day-16.js
@@ -19,6 +19,69 @@ function threeInOne(arr) {
 
 
 // https://www.codewars.com/kata/572df796914b5ba27c000c90
+function createArrFromRepeatNumber(number, amount) {
+  const arr = [];
+  for (let i = 0; i < amount; i += 1) {
+    arr.push(number);
+  }
+  return arr;
+}
+
+function sortIt(arr) {
+  // copy array to a new array
+  const arrCopy = arr.slice().sort((a, b) => b - a);
+  // create dictionary object that holds number of occurrence of each number
+  // fill dictionary given the array copy
+
+  const dictionary = {};
+
+  for (let i = 0; i < arrCopy.length; i++) {
+    let currentNumber = arrCopy[i];
+    if (dictionary[currentNumber] === undefined) {
+      dictionary[currentNumber] = 0;
+    }
+    dictionary[currentNumber] = dictionary[currentNumber] + 1;
+  }
+
+  // find maximum number of repeats
+  let maxRepeatNumber = 0;
+  let resultArr = [];
+  for (const [key, value] of Object.entries(dictionary)) {
+    if (value > maxRepeatNumber) {
+      maxRepeatNumber = value;
+    }
+  }
+  // for loop from 1 to maximum number of repeats
+  // create array from dictionary
+  let dictionaryArray = [];
+  for (let key in dictionary) {
+    if (dictionary.hasOwnProperty(key)) {
+      dictionaryArray.push([Number(key), dictionary[key]]);
+    }
+  }
+  for (
+    let numberOfRepeat = 1;
+    numberOfRepeat <= maxRepeatNumber;
+    numberOfRepeat += 1
+  ) {
+    // loop all keys from 9 to 0
+    for (let j = dictionaryArray.length - 1; j >= 0; j--) {
+      // if current key value === current number of repeats (i)
+
+      const currentElement = dictionaryArray[j];
+      const currentValue = currentElement[1];
+      if (currentValue === numberOfRepeat) {
+        // result array append j number i times
+        const arrTemp = createArrFromRepeatNumber(
+          currentElement[0],
+          numberOfRepeat
+        );
+        resultArr = resultArr.concat(arrTemp);
+      }
+    }
+  }
+  return resultArr;
+}
 
 
 // https://www.codewars.com/kata/572fdeb4380bb703fc00002c
