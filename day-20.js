6f62fa7da53e0b58beb0bd58236ba1e2ca92759b
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-02 21:46:08 +0300

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -1,4 +1,5 @@
 // https://www.codewars.com/kata/573bca07dffc1aa693000139/train/javascript
+var regex=/^-{0,1}9.*0{4,}$/
 
 
 // https://www.codewars.com/kata/573d11c48b97c0ad970002d4/train/javascript
