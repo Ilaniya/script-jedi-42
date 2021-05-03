9dab33631e1a024085e106cba77a6a48e4ea827c
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-03 17:01:15 +0300

    Update day-21.js

--- day-21.js
+++ day-21.js
@@ -3,10 +3,10 @@ var regex=/\b(\w?)(\w?)(\w)\w{0,1}\3\2\1\b/g
 
 
 // https://www.codewars.com/kata/573fb9223f9793e485000453/train/javascript
-var regex = /\d{3}(?!\$)/g;
+const regex = /\d{3}(?!\$)/g;
 function addCommas(money, regex) {
-  const strArr = money.split("").reverse().join("");
-  const replaceStr = strArr.replace(regex, (x) => x + ",");
-  const resultArr = replaceStr.split("").reverse().join("");
-  return resultArr;
+  const reverseStr = money.split("").reverse().join("");
+  const replaceStr = reverseStr.replace(regex, (x) => x + ",");
+  const resultStr = replaceStr.split("").reverse().join("");
+  return resultStr;
 }
