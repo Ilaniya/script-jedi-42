75cb9380289301cdd5f415b3285c7f0f3d77a458
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-03 13:11:53 +0300

    Update day-21.js

--- day-21.js
+++ day-21.js
@@ -1,3 +1,5 @@
-// https://www.codewars.com/kata/573fb9223f9793e485000453
-
 // https://www.codewars.com/kata/573e6831e3201f6a9b000971/train/javascript
+var regex=/\b(\w?)(\w?)(\w)\w{0,1}\3\2\1\b/g
+
+
+// https://www.codewars.com/kata/573fb9223f9793e485000453/train/javascript
