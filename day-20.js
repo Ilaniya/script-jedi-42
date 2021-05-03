c0602361dc67ba51be74dda6e9b23e19b0c51d26
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-03 11:20:52 +0300

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -3,5 +3,4 @@ var regex=/^-{0,1}9.*0{4,}$/
 
 
 // https://www.codewars.com/kata/573d11c48b97c0ad970002d4/train/javascript
-var regex=/(https?:\/\/[a-zA-Z0-9.]{1,}(.com)|(.net))+/gi 
-// чатково не правильно працює, поки що не можу зловити чому
+var regex=/(https?:\/\/[a-zA-Z0-9.]{1,}\.(net|com))+/gi
