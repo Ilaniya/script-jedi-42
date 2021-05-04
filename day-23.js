f85526ef5577ec4b74da9e75fec8410671e60641
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-04 22:28:16 +0300

    Update day-23.js

--- day-23.js
+++ day-23.js
@@ -1,3 +1,6 @@
+// https://www.codewars.com/kata/540de1f0716ab384b4000828/train/javascript
+
+
 // https://www.codewars.com/kata/5258b272e6925db09900386a/train/javascript
 function numbers(...arr) {
   let isArrNumeric = true;
