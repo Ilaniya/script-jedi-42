3421702b26c1f060d93f96810a0f132c8139ee94
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-04 21:45:43 +0300

    Create day-22.js

---
+++ day-22.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/559f860f8c0d6c7784000119/train/javascript
+function anyArrows(arrows) {
+  return arrows.some((oneArrow) => !oneArrow.damaged);
+}
+
+// https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
