4899408d6d282a0c60b069afdd1c373074d56e40
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-06 13:32:40 +0300

    Create day-25.js

---
+++ day-25.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/53d4eec01f1a9b3020000786/train/javascript
+var name = 'The Window';
+
+var alpha = {
+    name : 'My Alpha',
+    getNameFunc : function(){
+    return function(){
+        return this.name;
+        }.bind(this);
+    }
+};
+
+
+// https://www.codewars.com/kata/52d56d9c6b02b2fa9a0006d9/train/javascript
