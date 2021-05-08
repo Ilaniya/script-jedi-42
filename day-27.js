475101b5095ab5a3549c4390d5dc411a4fb96fe9
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-08 20:01:20 +0300

    Update day-27.js

--- day-27.js
+++ day-27.js
@@ -11,3 +11,13 @@ function buildFun(n) {
 }
 
 // https://www.codewars.com/kata/shifty-closures/train/javascript
+let name;
+const greet_abe = function () {
+  name = "Abe";
+  return "Hello, " + name + "!";
+};
+
+const greet_ben = function () {
+  name = "Ben";
+  return "Hello, " + name + "!";
+};
