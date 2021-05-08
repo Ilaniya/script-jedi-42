14e8c40e48a412728f6a157b366bae8c847c8810
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-08 20:32:45 +0300

    Update day-28.js

--- day-28.js
+++ day-28.js
@@ -1,4 +1,8 @@
 // https://www.codewars.com/kata/53844152aa6fc137d8000589/train/javascript
-
+function always(n) {
+  return function () {
+    return n;
+  };
+}
 
 // https://www.codewars.com/kata/59bdbe9d46038724ca0000b9/train/javascript
