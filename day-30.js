02148a1414c18907a14cc24a757495e9e4a511f9
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-12 14:31:52 +0300

    Update day-30.js

--- day-30.js
+++ day-30.js
@@ -1,4 +1,17 @@
 // https://www.codewars.com/kata/santaclausable-interface
+function isSantaClausable(obj) {
+    const hasHoProp =  typeof obj.sayHoHoHo ===  'function';
+    const hasGiftsProp = typeof obj.distributeGifts === 'function'; 
+    const hasChimneyProp = typeof obj.goDownTheChimney === 'function';  
+    if (hasHoProp && hasGiftsProp && hasChimneyProp){
+      return true;
+  }
+     return false;
+}
+
+function isSantaClausable(obj) {
+  return  typeof (obj.sayHoHoHo && obj.distributeGifts && obj.goDownTheChimney) == 'function';
+}
 
 //
 
