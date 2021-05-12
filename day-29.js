03136c2259925e0c11db83baa2583316bdcd5a42
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-12 13:16:52 +0300

    Update day-29.js

--- day-29.js
+++ day-29.js
@@ -1,4 +1,7 @@
 // https://www.codewars.com/kata/525a5b2185a9a4f5670006c1/train/javascript
+const myObj = {};
+myObj.__proto__ = MyObject.prototype;
+MyObject.call(myObj);
 
 
 // https://www.codewars.com/kata/558cb3df5f511f40d500001d/train/javascript
