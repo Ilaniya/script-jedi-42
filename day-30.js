806967651a272c90e54756294151ad94b3f0974f
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-12 21:08:25 +0300

    Update day-30.js

--- day-30.js
+++ day-30.js
@@ -14,5 +14,20 @@ function isSantaClausable(obj) {
 }
 
 // https://www.codewars.com/kata/52e5e86b2ff32887280002e4/train/javascript
+function Cylon(model) {
+  this.model = model;
+  this.attack = function () {
+    return "Destroy all humans!";
+  };
+}
+
+function HumanSkin(model) {
+  Cylon.call(this, model);
+  this.model = model;
+  this.infiltrate = function () {
+    return "Infiltrate the colonies";
+  };
+}
+HumanSkin.prototype = Object.create(Cylon.prototype);
 
 //
