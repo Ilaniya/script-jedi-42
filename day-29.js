f5b1be17df187cfd1031d32a7d88de643ca1cca4
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-13 14:06:19 +0300

    Update day-29.js

--- day-29.js
+++ day-29.js
@@ -5,3 +5,13 @@ MyObject.call(myObj);
 
 
 // https://www.codewars.com/kata/558cb3df5f511f40d500001d/train/javascript
+function nouveau(Constructor, ...args) {
+  const obj = Object.create(Constructor.prototype);
+  const inst = Constructor.apply(obj, args);
+  if (
+    inst &&
+    (typeof inst === "object" || typeof inst === "function")
+  ) {
+    return inst;
+  } else return obj;
+}
