e034384932a2f32f5d0098aa7ab5f44e248e206d
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-16 21:37:07 +0300

    Update day-34.js

--- day-34.js
+++ day-34.js
@@ -8,3 +8,10 @@ class Labrador extends Dog {
 }
 
 // https://www.codewars.com/kata/55a144eff5124e546400005a/train/javascript
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age; 
+      this.info =`${this.name}s age is ${this.age}`;       
+    }
+}
