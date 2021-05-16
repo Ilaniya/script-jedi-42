29e0d6b16902e70ded7c2d5fc850b2c2f97d6d19
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-16 20:56:35 +0300

    Create day-34.js

---
+++ day-34.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/547274e24481cfc469000416/train/javascript
+
+// https://www.codewars.com/kata/56ff9b53140fcca90b000530/train/javascript
+
+// https://www.codewars.com/kata/55a144eff5124e546400005a/train/javascript
