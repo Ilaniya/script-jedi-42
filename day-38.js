1df39d428bfda61a3e29db56c23b3aee8a77f97c
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-24 20:29:52 +0300

    Update day-38.js

--- day-38.js
+++ day-38.js
@@ -1,1 +1,14 @@
 //  https://www.codewars.com/kata/basics-generators-number-1/javascript
+function* generator(number) {
+  let counter = 1;
+  
+  while (number === undefined) {
+    yield counter++;
+  }
+  if (number !== undefined) {
+    counter = number;
+    yield counter;
+  }
+}
+
+// один тест не проходить, не розумію що не так
