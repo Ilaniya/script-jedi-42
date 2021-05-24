809b7c67b8e9daf5aa2ed29a01d513c6adbb1bfe
Євгенія Павлович <39456603+Ilaniya@users.noreply.github.com>
2021-05-24 11:31:04 +0300

    Update day-41.js

--- day-41.js
+++ day-41.js
@@ -1,5 +1,12 @@
 // https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/javascript 
-
+function queueTime(customers, n) {
+  const arrTemp = new Array(n).fill(0);
+  for (let time of customers) {
+    const idx = arrTemp.indexOf(Math.min(...arrTemp));
+    arrTemp[idx] += time;
+  }
+  return Math.max(...arrTemp);
+}
 
 //https://www.codewars.com/kata/567ed5db4089538eea000010/solutions/javascript
 function launchAll(launchMissile) {
